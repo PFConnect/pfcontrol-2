1f69c3489dd7d3ae18c1b94c0017f6323f3a65ed
PF-LufthansaVirtual <visual69030@gmail.com>
2025-10-04 22:26:03 +0200

    maxLength={16}

--- src/pages/Submit.tsx
+++ src/pages/Submit.tsx
@@ -388,6 +388,7 @@ export default function Submit() {
 											required
 											placeholder="e.g. DLH123"
 											className="flex items-center w-full pl-6 p-3 bg-gray-800 border-2 border-blue-600 rounded-full text-white font-semibold focus:outline-none focus:ring-2 focus:ring-blue-600 transition-all"
+											maxLength={16}
 										/>
 									</div>
 									<div>
