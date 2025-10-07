aba48c6e30ce0cd9991ce1f7fedc975598c7878e
dev-banane <jakob@uplift.de>
2025-10-07 07:44:23 +0200

    detectVPN.js aktualisieren

--- server/tools/detectVPN.js
+++ server/tools/detectVPN.js
@@ -1,18 +1,15 @@
 import axios from 'axios';
 
-export async function detectVPN(ip, req) {
+export async function detectVPN(req) {
+    let ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    // Skip localhost/private IPs
+    if (ip.startsWith('172.') || ip.startsWith('192.168.') || ip === '127.0.0.1') {
+        console.log(`Ignoring private or Docker IP: ${ip}`);
+        return false;
+    }
+
     try {
-        if (ip === '127.0.0.1' && req) {
-            ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-            console.log(`IP extrahiert aus Header: ${ip}`);
-        }
-
-        // Skip localhost/private IPs
-        if (ip === '127.0.0.1' || ip === '::1' || ip.startsWith('192.168.') || ip.startsWith('10.') || ip.startsWith('172.') || ip.startsWith('fc00::') || ip.startsWith('fe80::')) {
-            console.log(`VPN detection skipped for localhost/private IP: ${ip}`);
-            return false;
-        }
-
         const response = await axios.get(`http://ip-api.com/json/${ip}?fields=proxy,hosting`, {
             timeout: 5000
         });
