b827d49d2a3fe9f2f0a8cb96ac1d9c79f4f1c257
dev-banane <jakob@uplift.de>
2026-01-20 14:39:04 +0100

    fix: rating table type fix

--- server/db/types/connection/main/ControllerRatingsTable.ts
+++ server/db/types/connection/main/ControllerRatingsTable.ts
@@ -1,8 +1,10 @@
+import type { Generated } from 'kysely';
+
 export interface ControllerRatingsTable {
-  id: number;
+  id: Generated<number>;
   controller_id: string;
   pilot_id: string;
   rating: number;
   flight_id: string | null;
-  created_at: Date;
+  created_at: Generated<Date>;
 }
